ff6fcb38a7a6fa72dfb26b5609ae07f4a05eae79
Kishore Jagannath <kishorerj@google.com>
2025-06-24 08:25:59 +0530

    AGENT_ENGINE_ID replaced with placeholder

--- python/agents/image-scoring/deployment/test_deployment.py
+++ python/agents/image-scoring/deployment/test_deployment.py
@@ -2,7 +2,7 @@ import dotenv
 dotenv.load_dotenv()  # May skip if you have exported environment variables.
 from vertexai import agent_engines
 
-agent_engine_id = "4530454099367297024"
+agent_engine_id = ""# Replace with your AGENT_ENGINE_ID
 user_input = " Earth is further away from the Sun than Mars."
 
 agent_engine = agent_engines.get(agent_engine_id)
