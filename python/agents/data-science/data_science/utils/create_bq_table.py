6cac1fcc38541ceedb5013a2c95924f66c0b391c
DavidLee528 <tianhao.li@proton.me>
2025-06-25 10:49:30 +0000

    fix/update dataset name to use environment variable BQ_DATASET_ID

--- python/agents/data-science/data_science/utils/create_bq_table.py
+++ python/agents/data-science/data_science/utils/create_bq_table.py
@@ -87,7 +87,7 @@ def main():
     if not project_id:
         raise ValueError("BQ_PROJECT_ID environment variable not set.")
 
-    dataset_name = "forecasting_sticker_sales"
+    dataset_name = os.getenv("BQ_DATASET_ID")
     train_csv_filepath = "data_science/utils/data/train.csv"
     test_csv_filepath = "data_science/utils/data/test.csv"
 
