a4da6efb159c11de4982a659a984c7deaffbd071
Anuj Gupta <anuj@dream11.com>
2025-07-14 17:23:43 +0530

    removed unused code

--- python/agents/capital-guess-game/capital_guess_game/agent.py
+++ python/agents/capital-guess-game/capital_guess_game/agent.py
@@ -15,21 +15,6 @@ from .config import LOGGING_LEVEL
 logger = logging.getLogger(__name__)
 logger.setLevel(LOGGING_LEVEL)
 
-# capital_game_orchestrator_agent = Agent(
-#     name="capital_game_orchestrator_agent",
-#     model=AI_MODEL,
-#     generate_content_config=types.GenerateContentConfig(temperature=0.1),
-#     instruction="""
-#     You are a game orchestrator agent.
-#     """,
-#     description="You are the Capital Game Agent responsible for orchestrating the gameplay.",
-#     sub_agents=[
-#         gameplay_workflow_agent,
-#     ],
-#     before_model_callback=before_model_cb,
-#     before_agent_callback=before_agent_cb
-# )
-
 
 root_agent = Agent(
     model=AI_MODEL,
@@ -38,9 +23,6 @@ root_agent = Agent(
     description="Let's the user choose a game to play",
     instruction=GAME_ARCADE_AGENT_INSTR,
     include_contents="none",
-    # sub_agents=[
-    #     capital_game_orchestrator_agent,
-    # ],
     sub_agents=[
         capital_guess_workflow_agent,
     ],
