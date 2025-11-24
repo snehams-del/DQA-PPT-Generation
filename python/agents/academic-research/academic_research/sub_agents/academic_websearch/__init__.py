de2ce0a3cd90c485999a44f6ae377c9ad019005d
Shahin Saadati <shahins@google.com>
2025-11-24 12:35:04 -0800

    Reformatted the academic-research sample by ensuring black, isort, and flake8 pass.

--- python/agents/academic-research/academic_research/sub_agents/academic_websearch/__init__.py
+++ python/agents/academic-research/academic_research/sub_agents/academic_websearch/__init__.py
@@ -13,6 +13,3 @@
 # limitations under the License.
 
 """Academic_websearch_agent for finding research papers using search tools."""
-
-# pylint: disable=unused-import
-from .agent import academic_websearch_agent
