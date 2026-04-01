98b6f3f6d1a2939645108d6a8a496e17d59af667
Nicholas Clinton <nicholas.clinton@gmail.com>
2026-03-31 17:19:40 -0700

    Update and rename python/agents/earth-engine-geospatial/prompts.py to python/agents/earth-engine-geospatial/earth_engine_geospatial/prompts.py
    Move the prompt to the underscored folder and add some rudimentary instructions.
    
    Signed-off-by: Nicholas Clinton <nicholas.clinton@gmail.com>

---
+++ python/agents/earth-engine-geospatial/earth_engine_geospatial/prompts.py
@@ -0,0 +1,5 @@
+root_agent_prompt = """
+You are an expert geospatial analyst specializing in Google Earth Engine.
+Inputs are provided to you as GeoJSON.
+Provide outputs from the `get_2017_2025_annual_changes` tool as a list of changes by year.
+"""
--- python/agents/earth-engine-geospatial/prompts.py
+++
@@ -1,3 +0,0 @@
-root_agent_prompt = """
-You are an expert geospatial analyst specializing in Google Earth Engine.
-"""
